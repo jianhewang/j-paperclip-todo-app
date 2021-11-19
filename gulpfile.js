f7eac36a68fb509db3f4e645635e06a21ccf3f8a
jianhewang <kuyasda@hotmail.com>
2021-11-18 20:35:01 -0500

    updated gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,17 +1,21 @@
 // node common js modules
 // browser ES modules
 // import src from gulp
-const {src, dest, series} = require('gulp')
+const {src, dest} = require('gulp')
 
-const static = function(){
-   return( src('src/static/**')
-    .pipe(dest('dist/static')))
+const static = function(cb) {
+    //task
+    // src().pipe(`plugin`).pipe(`compress`).pipe(dest())
+    // any folder, any type - src/static/*.*/*.*
+    src('src/static/data/*.*')
+        .pipe(dest('dist/data'))
 
+    // call the callback
+    cb()
 }
 
 function redirect(){
     return (src('./_redirects').pipe(dest('./dist')))
 }
 
- 
 exports.default = series(static, redirect)
